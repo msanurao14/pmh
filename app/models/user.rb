16e160a4f42298d80e75983d082a5fcb558d5a5c
Anu <msanurao@yahoo.com>
2014-05-15 12:12:20 -0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ActiveRecord::Base
 
 
 
-   #validates :name, presence: true, length: { maximum: 15 }
+   validates :name, presence: true, length: { maximum: 15 }
 
-   #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-   #validates :email, presence: true, 
-#		     format:     { with: VALID_EMAIL_REGEX },
-#		     uniqueness: { case_sensitive: false }
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+   validates :email, presence: true, 
+		     format:     { with: VALID_EMAIL_REGEX },
+		     uniqueness: { case_sensitive: false }
    has_secure_password
+   validates :password_confirmation, presence: true
    validates :password, length: { minimum: 6 }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  
+  resources :users
+#  get 'users/new'
 
   root  'static_pages#home'
  # match '/help', to: 'static_pages#help', via: 'get'
